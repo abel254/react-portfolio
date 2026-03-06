8fa6fb5f420ec0a642337b0cf562e167d6695e29
abel254 <abelmuema353@gmail.com>
2026-03-06 14:40:07 +0300

    project updates

--- src/Components/Projects/ProjectCard.jsx
+++ src/Components/Projects/ProjectCard.jsx
@@ -1,21 +1,27 @@
 import React from "react";
-import personalBlog from "../../assets/personal-blog.png"
 
-const ProjectCard = ({ title, main }) => {
+
+const ProjectCard = ({ title, main, image, demo_link, source_link, showDemo= true}) => {
   return (
     <div className="p-3 md:p-6 flex flex-col w-80 bg-[#0c0e19] shadow-xl shadow-slate-900 rounded-2xl">
-      <img src={personalBlog} alt="personal-blog" />
-      <h3 className="px-4 text-xl md:text-2xl font-bold leading-normal">
+      <img src={image} alt="personal-blog" />
+      <h3 className="px-4 text-xl md:text-2xl font-bold leading-normal pt-2">
         {title}
       </h3>
       <p className="px-4 text-sm md:text-md leading-tight py-2">{main}</p>
-      <div className="mt-2 p-2 md:p-4 flex gap-2 md:gap-4">
-        <button className="md:mt-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697]">
+      <div className="p-2 md:p-4 flex gap-2 md:gap-4">
+        {showDemo && (
+          <a href={demo_link} target="_blank">
+           <button className="md:mt-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697]">
           Demo
         </button>
-        <button className="md:mt-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697]">
+        </a>
+        )}
+        <a href={source_link} target="_blank">
+          <button className="md:mt-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697]">
           Source Code
         </button>
+        </a>
       </div>
     </div>
   );
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import ProjectCard from "./ProjectCard";
+import personalBlog from "../../assets/personal-blog.png"
+import wiseMysteries from "../../assets/wise-mysteries.png"
+import amazonClone from "../../assets/amazon-clone.png"
+import reactProfile from "../../assets/react-profile.png"
 
 const Projects = () => {
   return (
@@ -8,16 +12,34 @@ const Projects = () => {
       <div className="py-12 px-8 flex flex-wrap gap-5 justify-center md:justify-start">
         <ProjectCard
           title="Blogging Website"
-          main="This is a blogging website created with React js and used some component library"
+          main="This is a blogging website created with TypeScript and used some component library"
+          image={personalBlog}
+          demo_link="https://personal-blog-ochre.vercel.app/"
+          source_link="https://github.com/abel254/personal-blog"
+        />
+         <ProjectCard
+          title="Wise Mysteries"
+          main="This is an affiliate website created with React js which saves user data and sends them emails."
+          image={wiseMysteries}
+           demo_link="https://wisemysteriesdevotion.vercel.app/"
+          source_link="https://github.com/abel254/wisemysteriesdevotion"
         />
          <ProjectCard
           title="Amazon Clone"
-          main="This is a blogging website created with React js and used some component library"
+          main="This is a sample clone of the amazon website created with TypeScript. It can add items to cart and calculate the total."
+          image={amazonClone}
+           demo_link="https://amazon-clone-sigma-ivory-29.vercel.app"
+          source_link="https://github.com/abel254/amazon-clone"
         />
-         <ProjectCard
-          title="WiseMysteries"
-          main="This is a blogging website created with React js and used some component library"
+        <ProjectCard
+          title="Profile"
+          main="This is the current profile created with React js and Tailwind css"
+          image={reactProfile}
+           demo_link="https://react-portfolio-eudk.vercel.app"
+          source_link="https://github.com/abel254/react-portfolio"
+          showDemo={false}
         />
+       
       </div>
     </div>
   );
